505ed520cdfb4c78203dfbf92e6ff458f66d3df4
Daniel Lebrecht <dlebrecht@gmail.com>
2017-11-24 20:39:51 +0100

    train

--- listener.ts
+++ listener.ts
@@ -1,7 +1,6 @@
 import { socket as Socket } from 'zeromq'
 import { Transaction } from 'bitcoinjs-lib'
 import * as RpcClient from 'bitcoin-core'
-import * as sio from 'socket.io'
 import { Observable, Subscriber } from 'rxjs'
 const reverse: (b: Buffer) => Buffer = require("buffer-reverse")
 
@@ -19,7 +18,7 @@ const getFee = (tx_bytes: Buffer) =>
     .map(tx => ({
       txid: tx.getId(),
       size: tx.byteLength(),
-      rbf: tx.ins.every(y => y.sequence < 0xffffffff - 1),
+      rbf: tx.ins.every(y => y.sequence < 0xffffffff - 1), 
       ins: tx.ins.map(y => ({
         prev_txid: reverse(y.hash).toString('hex'),
         prev_index: y.index,
@@ -30,20 +29,21 @@ const getFee = (tx_bytes: Buffer) =>
       curr.ins.map(prev =>
         Observable.fromPromise(rpc.getRawTransaction(prev.prev_txid)
           .then(x => x)
-          .catch(err => err))
+          .catch(e => e))
           .flatMap((prev_txid: string): Observable<Tx> =>
             Observable.fromPromise(rpc.decodeRawTransaction(prev_txid)
               .then(x => x)
               .catch(err => err)))
           .map(prev_tx => prev_tx.vout[prev.prev_index].value * 1e8))
-        .reduce((acc1, x) =>
-          x.reduce((acc2, y) => y + acc2, 0)
-            .flatMap(z => acc1.map(a => a + z)), Observable.of(0))
+        .reduce((acc1, x) => x
+          .reduce((acc2, y) => y + acc2, 0)
+          .flatMap(z => acc1.map(a => a + z)), Observable.of(0))
         .map(prev_value => ({
+          feeSatoshiPerByte: (prev_value - curr.value) / curr.size,
           txid: curr.txid,
-          prev_txids: curr.ins.map(x => x.prev_txid),
           rbf: curr.rbf,
-          feeSatoshiPerByte: (prev_value - curr.value) / curr.size
+          size: curr.size,
+          prev_txids: curr.ins.map(x => x.prev_txid),
         })))
     .retryWhen(errors => errors.delay(5000))
 
@@ -65,29 +65,23 @@ const rawtxsocket$: Observable<any> =
     return () => socket.close()
   })
 
-const n = 5
+
+// TODO: since acc is a sorted list, instead of inserting x to the top and full sort
+// again, insert x in the correct position directly. binary search and insert
+const n = 3
 const transaction$ =
   rawtxsocket$
     .flatMap((message) => getFee(message))
-    .scan(
-    (acc, x) => acc.length < n
+    .scan((acc, x) => (acc.length < n
       ? [x, ...acc]
-      : x > acc[-1]
-        ? [...acc.slice(0, n - 1), x]
-        : acc
-    , [])
-    // get size from acc to drop elements that are far off
-
+      : (x.feeSatoshiPerByte > acc[n - 1].feeSatoshiPerByte
+        ? [x, ...acc.slice(0, n - 1)]
+        : acc)), [])
+    .distinctUntilChanged()
     .map(txs =>
       txs.sort((a, b) => b.feeSatoshiPerByte - a.feeSatoshiPerByte))
 
 // flatMap mempool_node$ here
-
-// const sortedTransactions$ =
-//   transaction$
-//     // .toArray()
-//     .map(txs =>
-//       txs.sort((a, b) => b.feeSatoshiPerByte - a.feeSatoshiPerByte))
 
 const mempool_node$: Observable<string[]> = Observable.fromPromise(
   rpc.getRawMemPool()
@@ -95,16 +89,19 @@ const mempool_node$: Observable<string[]> = Observable.fromPromise(
     .catch(e => e)
 )
 
-const mempuller$ =
-  Observable.timer(10000, 5000)
-    .flatMap((_) => mempool_node$)
+// TODO: merge this observable to one that emits when a new block arrives
+const mempuller$ = Observable.timer(0, 5000)
+  .flatMap((_) => mempool_node$)
 // .flatMap(txids => txids
 //   .map(txid => transactions$
 //     .filter(tx => tx.txid === txid)
 //   )).mergeAll())
 
 transaction$
-  .subscribe((x) => console.log('\n\nnew\n\n\n', x))
+  .subscribe(
+  (x) => console.log('\n\nnew\n\n\n', x),
+  console.error,
+  () => console.log('finished'))
 // .combineLatest(mempuller$, (txs, txids) => txs.map)
 // mempool$.combineAll()
 
