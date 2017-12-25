97dfb03ff179a0e87410377e1e89548d2f2c8178
Daniel Lebrecht <dlebrecht@gmail.com>
2017-12-25 19:43:08 +0100

    fixing client so that it can connects from anywhere

--- client.ts
+++ client.ts
@@ -1,11 +1,19 @@
 import { Client } from 'thruway.js'
-import { range } from './listener'
 import { Observable } from 'rxjs'
-
-const wamp = new Client('ws://localhost:8080/ws', 'realm1')
+const wamp = new Client('http://159.100.247.219:8080/ws', 'realm1')
 const minedTxSummary$ = wamp.topic('com.buffered.minedtxssummary')
   .flatMap(x => x.args)
-const baseStr = 'com.buffered.getfee'
-const fees = range.map(x => wamp.topic(baseStr + x).flatMap(y => y.args))
 
-Observable.merge(...fees, minedTxSummary$).subscribe(x => console.dir(x))
+const feeDiff$ = wamp.topic('com.buffered.feediff')
+  .flatMap(y => y.args)
+
+Observable.merge(
+  minedTxSummary$,
+  feeDiff$
+)
+  .subscribe(
+  x => console.dir(x),
+  err => console.error(err),
+  () => console.log('finished')
+  )
+
