5667745b40d7dfdf4a27714cbe369d4c889cce0e
Daniel Lebrecht <dlebrecht@gmail.com>
2017-12-26 04:30:51 +0100

    best deal

--- client.ts
+++ client.ts
@@ -1,17 +1,19 @@
 import { Client } from 'thruway.js'
 import { Observable } from 'rxjs'
 const wamp = new Client('http://159.100.247.219:8080/ws', 'realm1')
-const minedTxSummary$ = wamp.topic('com.buffered.minedtxssummary')
+const minedTxSummary$ = wamp.topic('com.fee.minedtxssummary')
   .flatMap(x => x.args)
 
-const feeDiff$ = wamp.topic('com.buffered.feediff')
+// const feeDiff$ = wamp.topic('com.buffered.feediff')
+//   .flatMap(y => y.args)
+
+const minDiff$ = wamp.topic('com.fee.mindiff')
   .flatMap(y => y.args)
 
 Observable.merge(
   minedTxSummary$,
-  feeDiff$
-)
-  .subscribe(
+  minDiff$
+).subscribe(
   x => console.dir(x),
   err => console.error(err),
   () => console.log('finished')
